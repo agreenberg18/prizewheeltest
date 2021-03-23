1a800862ab6c99aa485fadaa55ac590e8fd4d3bc
Andrew Greenberg <andrewgreenberg@Andrews-MacBook-Air.local>
2021-03-23 16:00:50 -0400

    root commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <canvas id="canvas" width="1000" height="600">
+    Canvas not supported, use another browser.
+  </canvas>
+  <button @click="startAnimation">Spin the wheel</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TweenMax from "@/assets/TweenMax.min.js";
+import Winwheel from "winwheel/lib/Winwheel.js";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      theWheel: null,
+    };
+  },
+  mounted() {
+    this.theWheel = new Winwheel({
+      numSegments: 4,
+      outerRadius: 170,
+      segments: [
+        { fillStyle: "#eae56f", text: "Prize One" },
+        { fillStyle: "#89f26e", text: "Prize Two" },
+        { fillStyle: "#7de6ef", text: "Prize Three" },
+        { fillStyle: "#e7706f", text: "Prize Four" },
+      ],
+      // Note animation properties passed in constructor parameters.
+      animation: {
+        type: "spinToStop", // Type of animation.
+        duration: 5, // How long the animation is to take in seconds.
+        spins: 8,
+        "callbackFinished": this.alertPrize
+      },
+    });
+  },
+  methods: {
+    startAnimation() {
+      this.theWheel.startAnimation();
+    },
+    alertPrize(){
+      alert('hello')
+      console.log('testing')
+    }
+  },
+};
 </script>
+
+<style>
+</style>
